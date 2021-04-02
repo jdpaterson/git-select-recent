7aaf5499035be0766a7b09db66238fc4c06ce633
J Paterson <contact@jpatersondev.com>
2021-04-02 08:13:08 -0700

    add remote branches function

--- git-select-recent.sh
+++ git-select-recent.sh
@@ -1,14 +1,30 @@
-i=0
-branches=()
-while read entry
-do
-    eval "$entry"
-    branches+=($ref)
-    let i++
-done < <(git for-each-ref --format="ref=%(refname:short)" --count="5" --sort=-committerdate refs/heads)
+git-select-recent(){
+    PS3="Enter #: "
+    BRANCH_COUNT=${1:-10}
+    SOURCE=${2:-refs/heads}
+    FORMAT=${3:-short}
+    i=0
+    branches=()
+    while read entry
+    do
+        eval "$entry"
+        branches+=($ref)
+        let i++
+    done < <(
+        git for-each-ref \
+            --format="ref=%(refname:$FORMAT)" \
+            --count="$BRANCH_COUNT" \
+            --sort=-committerdate \
+            --no-contains origin \
+            $SOURCE
+    )
 
-select branch in ${branches[@]};
-do
-    git checkout $branch
-    break;
-done
+    select branch in ${branches[@]};
+    do
+        git checkout $branch
+        break;
+    done
+}
+git-select-recent-remote(){
+    git-select-recent ${1:-10} refs/remotes strip=3
+}
